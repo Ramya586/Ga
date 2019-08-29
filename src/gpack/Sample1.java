47e85aaa681280e9e8e5fe12eca8ca788925bf6e
Ramya586 <pradeepisces@gmail.com>
2019-08-29 20:12:03 +0530

    design

---
+++ src/gpack/Sample1.java
@@ -0,0 +1,12 @@
+package gpack;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+System.out.println("hiiiiiii");
+
+	}
+
+}
